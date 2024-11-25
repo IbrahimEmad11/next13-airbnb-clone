28396fd6a8407a50b3fe6ed2126c1690455f4b95
Ibrahim Emad <ibrahimemad.2002x@gmail.com>
2024-11-25 18:16:13 +0200

    setup environment

---
+++ airbnb-clonee/app/layout.tsx
@@ -0,0 +1,22 @@
+ import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
+import './globals.css'
+
+const nunito = Nunito({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Airbnb',
+  description: 'Book unique homes and experiences all over the world.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={nunito.className}>{children}</body>
+    </html>
+  )
+}
